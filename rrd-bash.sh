d1bdea381befd9a42c9c2b538776c10fde4dd5d8
David Transue <dtengineer23@gmail.com>
2017-01-25 09:28:15 -0700

    Script called to crontab to update rrd - comments revised

--- rrd-bash.sh
+++ rrd-bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #script to load data into rrd archive on edison.
-#script is called by crontab ever 5 minutes: */5 * * * * ~/rrd-bash.sh
 python /home/root/edisoni-sensors/photocell-rrd-update.py
- 
+sleep 5
+rrdtool dump /home/root/edisoni-sensors/photocell.rrd /home/root/edisoni-sensors/photocell.xml
