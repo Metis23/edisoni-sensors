cf0fafbd9da212e0b2f219f27c687bef524fc5d6
root <root@debian.bimmoth>
2017-01-26 10:57:10 -0700

    Server side - Server uses scp to copy dumped database (.xml) from Edison and regenerates the .rrd database

--- scp_rrd_restore.sh
+++ scp_rrd_restore.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #This script is called by crontab (server) every 5 minutes:
-# */5 * * * *  /usr/local/bin/rrdtranslate.sh
-#photocell.xml is a translation of Edisoni's photocell.rrd file. Server cannot
-#use Edisoni's .rrd because Edisoni is 32-bit.  Edisoni dumps it rrd into
-#an .xml file every 5 minutes.  Edisoni home directory is mounted
-# on server by: #sshfs root@192.168.1.20:/home/edisoni/ edroot
-#The following command converts the .xml into an .rrd file that the server can use.
+# */5 * * * *  /usr/local/bin/scp_rrd_restore.sh
+#photocell.xml is a translation of Edisoni's photocell.rrd file created
+#by the edison. The server cannot use Edisoni's .rrd because Edisoni uses
+#a different architecture.  Edisoni dumps it's the rrd database into
+#an .xml file every 5 minutes. The server then copies the .xml file
+#from the Edison to the server with this line:
+
 scp root@192.168.1.20:/home/root/edisoni-sensors/photocell.xml /home/edisoni
 sleep 1
+
+#The following command converts the .xml into an .rrd file that the server can use. This .rrd will be called from the cgi script to make charts of the data.
 rrdtool restore -f /home/edisoni/photocell.xml /var/log/photocell2.rrd
